681f3e49daf87f7df47b3db4032a38c56922f8f1
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-08-23 17:18:36 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '5M-CodeX | TheStoicBear'
+description '5M-CodeX Water Tanker Refill Script'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
 
