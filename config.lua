897daab4504e58e48cb5425f80c01eea291cbb25
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-08-23 17:17:12 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,16 @@
-
+Config = {
+-- Filling configuration
+fillDuration = 10000, -- 10 seconds in milliseconds
+slowdownFactor = 0.5,
+-- Allowed vehicle models
+allowedVehicles = {
+  "tanker",
+  "firetruk"
+},
+-- Refill locations
+refillLocations = {
+    { x = 100.0, y = -200.0, z = 30.0 }, -- Example location 1
+    { x = -300.0, y = 500.0, z = 20.0 }, -- Example location 2
+    -- Add more refill locations as needed
+   }
+}
